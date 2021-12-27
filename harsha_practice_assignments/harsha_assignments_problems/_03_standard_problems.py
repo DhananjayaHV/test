e811e8b9f1d815b3ca86a11b4d5a07dcd833f09c
DhananjayaHV <95115126+DhananjayaHV@users.noreply.github.com>
2021-12-27 17:24:51 +0530

    27-12-21

--- harsha_practice_assignments/harsha_assignments_problems/_03_standard_problems.py
+++ harsha_practice_assignments/harsha_assignments_problems/_03_standard_problems.py
@@ -1,10 +1,22 @@
-number = int(input("Enter a positive integer: "))
-rev = 0
-while(number!=0):
-    digit = number%10
-    print("digit ",digit)
-    rev = (rev*10)+digit
-    print("Rev ",rev)
-    number = number//10
-    print("Number ",number)
-print(rev)
+
+class A:
+
+    def __init__(self):
+        self.number=int(input("Enter a positive integer: "))
+
+
+    def rev_nubr(self):
+        rev = 0
+        number=self.number
+        while(number!=0):
+            digit = number%10
+            print("digit ",digit)
+            rev = (rev*10)+digit
+            print("Rev ",rev)
+            number = number//10
+            print("Number ",number)
+        print(rev)
+
+
+a=A()
+a.rev_nubr()
